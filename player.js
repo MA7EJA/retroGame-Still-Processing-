0c12cc95a3b22ad8cbf9f1d7582d859d277db42b
MA7EJA <matejajovanovic63@gmail.com>
2024-06-09 17:55:41 +0200

    feat: Add basic player movement

--- player.js
+++ player.js
@@ -7,10 +7,24 @@ export class Player {
         this.height = this.spriteHeight * 2;
         this.x = 0;
         this.y = this.game.height - this.height;
+        this.vy = 0;
+        this.weight = 1;
         this.image = document.getElementById('player')
+        this.speed = 0;
+        this.maxSpeed = 10;
     }
-    update(){
-        //this.x++
+    update(input){
+        this.x += this.speed;
+        if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
+        else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+        if(this.x < 0) this.x = 0;
+        if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+
+        if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
+        this.y += this.vy;
+        if(!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
     }
     draw(context){
         context.imageSmoothingEnabled = false;
@@ -18,5 +32,8 @@ export class Player {
         context.mozImageSmoothingEnabled = false;
         context.msImageSmoothingEnabled = false;
         context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+    }
+    onGround(){
+        return this.y >= this.game.height - this.height;
     }
 }

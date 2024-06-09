9c1617d198ce60513ed61479c0ebadf3a970190b
MA7EJA <matejajovanovic63@gmail.com>
2024-06-09 14:47:28 +0200

    feat: Add key event listeners in inputhandler module

--- input.js
+++ input.js
@@ -3,6 +3,28 @@ export class InputHandler{
         this.keys = [];
         window.addEventListener('keydown', (e) => {
             console.log(e.key);
+            if (
+              ( e.key === "ArrowDown" ||
+                e.key === "ArrowUp" ||
+                e.key === "ArrowLeft" ||
+                e.key === "ArrowRight" ||
+                e.key === 'x'
+              ) &&
+              this.keys.indexOf(e.key) === -1
+            ) {
+              this.keys.push(e.key);
+            }
         });
+        window.addEventListener('keyup', (e) => {
+            if (
+              e.key === "ArrowDown" ||
+              e.key === "ArrowUp" ||
+              e.key === "ArrowLeft" ||
+              e.key === "ArrowRight" ||
+              e.key === "x"
+            ) {
+              this.keys.splice(this.keys.indexOf(e.key), 1);
+            }
+        })
     }
 }

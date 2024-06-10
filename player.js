66833c5148d62e884c940fe30215ca697305349b
MA7EJA <matejajovanovic63@gmail.com>
2024-06-10 12:48:34 +0200

    feat: Add basic sprite fps control

--- player.js
+++ player.js
@@ -1,61 +1,78 @@
 import { Idle, Running, Jumping, Falling } from "./playerStates.js";
 
 export class Player {
-    constructor(game){
-        this.game = game;
-        this.spriteWidth = 48;
-        this.spriteHeight = 48;
-        this.width = this.spriteWidth * 2;
-        this.height = this.spriteHeight * 2;
-        this.x = 0;
-        this.y = this.game.height - this.height;
-        this.vy = 0;
-        this.weight = 1;
-        this.image = document.getElementById('player')
-        this.frameX = 0;
-        this.frameY = 0;
-        this.speed = 0;
-        this.maxSpeed = 10;
-        this.states = [new Idle(this), new Running(this), new Jumping(this), new Falling(this)];
-        this.currentState = this.states[0];
-        this.currentState.enter();
-    }
-    update(input){
-        this.currentState.handleInput(input);
-        this.x += this.speed;
-        if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
-        else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
-        else this.speed = 0;
-        if(this.x < 0) this.x = 0;
-        if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+  constructor(game) {
+    this.game = game;
+    this.spriteWidth = 48;
+    this.spriteHeight = 48;
+    this.width = this.spriteWidth * 2;
+    this.height = this.spriteHeight * 2;
+    this.x = 0;
+    this.y = this.game.height - this.height;
+    this.vy = 0;
+    this.weight = 1;
+    this.image = document.getElementById("player");
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame = 5;
+    this.fps = 20;
+    this.frameInterval = 1000/this.fps;
+    this.frameTimer = 0; 
+    this.speed = 0;
+    this.maxSpeed = 10;
+    this.states = [
+      new Idle(this),
+      new Running(this),
+      new Jumping(this),
+      new Falling(this),
+    ];
+    this.currentState = this.states[0];
+    this.currentState.enter();
+  }
+  update(input, deltaTime) {
+    this.currentState.handleInput(input);
+    this.x += this.speed;
+    if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
+    else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
+    else this.speed = 0;
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
 
-        //if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
-        this.y += this.vy;
-        if(!this.onGround()) this.vy += this.weight;
-        else this.vy = 0;
+    this.y += this.vy;
+    if (!this.onGround()) this.vy += this.weight;
+    else this.vy = 0;
+
+    if(this.frameTimer > this.frameInterval){
+      this.frameTimer = 0;
+      if(this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    }else {
+        this.frameTimer += deltaTime;
     }
-    draw(context){
-        context.imageSmoothingEnabled = false;
-        context.webkitImageSmoothingEnabled = false;
-        context.mozImageSmoothingEnabled = false;
-        context.msImageSmoothingEnabled = false;
-        context.drawImage(
-          this.image,
-          this.frameX * this.spriteWidth,
-          this.frameY * this.spriteHeight,
-          this.spriteWidth,
-          this.spriteHeight,
-          this.x,
-          this.y,
-          this.width,
-          this.height
-        );
-    }
-    onGround(){
-        return this.y >= this.game.height - this.height;
-    }
-    setState(state){
-        this.currentState = this.states[state];
-        this.currentState.enter();
-    }
+  }
+  draw(context) {
+    context.imageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.mozImageSmoothingEnabled = false;
+    context.msImageSmoothingEnabled = false;
+    context.drawImage(
+      this.image,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  onGround() {
+    return this.y >= this.game.height - this.height;
+  }
+  setState(state) {
+    this.currentState = this.states[state];
+    this.currentState.enter();
+  }
 }
